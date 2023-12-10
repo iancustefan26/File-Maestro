338d058540df1dc94eda1fa81ba9535981292803
stefaniancu26 <stefaniancu46@gmail.com>
2023-12-10 18:17:35 +0200

    i added the next icons and loaded them, all the thing are working and rendering correct, clicking and hovering color added

--- src/drawbars.cpp
+++ src/drawbars.cpp
@@ -6,7 +6,9 @@
 #define WINDOW_H 1080
 using namespace std::filesystem;
 
-void renderIcon(const std::string iconPath, sf::RenderWindow& window) {
+bool loaded = false;
+
+void renderIcon(const std::string iconPath, sf::RenderWindow& window, sf::Vector2f position) {
     sf::RectangleShape iconBox(sf::Vector2f(60.f, 60.f));
     sf::FloatRect iconBoxRect = iconBox.getGlobalBounds();
 
@@ -15,33 +17,27 @@ void renderIcon(const std::string iconPath, sf::RenderWindow& window) {
         sf::Texture iconTexture;
         iconTexture.loadFromImage(icon);
         sf::Sprite iconSprite(iconTexture);
-        iconSprite.setPosition(8.f, 10.f);
+        iconSprite.setPosition(position); ///inital 8.f, 10.f -> 70.f mai in dreapta(x)
         window.draw(iconSprite);
-        std::cout << "Loaded window icon..." << "\n";
+        if (!loaded) std::cout << "Loaded toolbar icon..." << "\n", loaded = true;
     }
     else {
-        std::cerr << "Error when loading the window icon!" << "\n";
+        if(!loaded) std::cerr << "Error when loading the toolbar icon!" << "\n", loaded = true;
         return;
     }
 }
 
-void drawToolbar(sf::RenderWindow& window) {
-	sf::Color defaultColor(51, 53, 54);
+void drawIconBoxes(sf::RenderWindow& window, const float index) {
+    sf::Color defaultColor(51, 53, 54);
     sf::Color hoverColor(7, 148, 224, 128);
     sf::Color clickedColor(224, 20, 75);
 
-    sf::RectangleShape toolbar(sf::Vector2f(window.getSize().x, 70.f));
     sf::RectangleShape iconBox(sf::Vector2f(70.f, 70.f));
-
-    toolbar.setPosition(0.f, 0.f);
-    toolbar.setFillColor(defaultColor);
-    iconBox.setPosition(0.f, 0.f);
+    iconBox.setPosition(0.f + index, 0.f);
     iconBox.setFillColor(defaultColor);
-
 
     sf::FloatRect iconBoxRect = iconBox.getGlobalBounds();
     sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-    sf::Image icon;
 
     if (iconBoxRect.contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y))) {
         iconBox.setFillColor(hoverColor);
@@ -51,7 +47,21 @@ void drawToolbar(sf::RenderWindow& window) {
     else {
         iconBox.setFillColor(defaultColor);
     }
-    window.draw(toolbar);
     window.draw(iconBox);
-    renderIcon("C:/PROIECT IP ORIGINAL/My Commander/assets/icons/refresh_icon.png", window);
+}
+
+void drawToolbar(sf::RenderWindow& window) {
+	sf::Color defaultColor(51, 53, 54);
+  
+    sf::RectangleShape toolbar(sf::Vector2f(window.getSize().x, 70.f));
+    toolbar.setPosition(0.f, 0.f);
+    toolbar.setFillColor(defaultColor);
+
+    window.draw(toolbar);
+    for (int i = 0; i < 4; ++i)
+        drawIconBoxes(window, i * 70);
+    renderIcon("C:/PROIECT IP ORIGINAL/My Commander/assets/icons/refresh_icon.png", window, sf::Vector2f(8.f, 10.f));
+    renderIcon("C:/PROIECT IP ORIGINAL/My Commander/assets/icons/terminal_icon.png", window, sf::Vector2f(78.f, 10.f));
+    renderIcon("C:/PROIECT IP ORIGINAL/My Commander/assets/icons/notepad_icon.png", window, sf::Vector2f(152.f, 10.f));
+    renderIcon("C:/PROIECT IP ORIGINAL/My Commander/assets/icons/dark_light_mode_icon.png", window, sf::Vector2f(222.f, 10.f));
 }
