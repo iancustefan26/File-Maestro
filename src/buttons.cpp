29ec5c9aaea3034bb482a4bd1b81772ccadb7c01
iancustefan26 <stefaniancu46@gmail.com>
2023-12-17 17:53:46 +0200

    I added text for all the down buttons for short commands

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -32,7 +32,26 @@ void makeButton(sf::RenderWindow& window, std::string name, float index) {
     else {
         iconBox.setFillColor(defaultColor);
     }
+
+    sf::Text buttonText;
+    sf::Font font;
+    if (!font.loadFromFile("C:/PROIECT IP ORIGINAL/My Commander/assets/fonts/aovel_sans.ttf")) {
+        std::cerr << "Couldn't load the font quicksand for diskspace";
+        return;
+    }
+    buttonText.setFillColor(sf::Color::White);
+    buttonText.setFont(font);
+    buttonText.setCharacterSize(24);
+    buttonText.setString(name);
+    //buttonText.setPosition(iconBox.getPosition().x + 45.f, iconBox.getPosition().y + 4.f);
+    sf::Vector2f boxPosition(iconBox.getPosition().x, iconBox.getPosition().y);
+    sf::Vector2f boxSize(iconBox.getSize().x, iconBox.getSize().y);
+    sf::Vector2f textPosition;
+    textPosition.x = boxPosition.x + (boxSize.x - buttonText.getGlobalBounds().width) / 2;
+    textPosition.y = boxPosition.y + (boxSize.y - buttonText.getGlobalBounds().height) / 2 - 5.f;
+    buttonText.setPosition(textPosition);
     window.draw(iconBox);
+    window.draw(buttonText);
     window.draw(line);
 }
 
